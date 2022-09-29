f6998fe993c545635d4911176d7d42c36a83877e
garg-vanshu786 <112563292+garg-vanshu786@users.noreply.github.com>
2022-09-29 22:04:28 +0530

    Updates

---
+++ precache-manifest.b32e19678e937fc5ee20bcfab0b7accc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7d129b3ca454835b5c9",
+    "url": "/textUtils/static/css/main.f7130c33.chunk.css"
+  },
+  {
+    "revision": "c7d129b3ca454835b5c9",
+    "url": "/textUtils/static/js/main.c7d129b3.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "9a7857878919569515e3",
+    "url": "/textUtils/static/js/runtime~main.9a785787.js"
+  },
+  {
+    "revision": "e327fc14749bfd1bde5f9d8f38bc558c",
+    "url": "/textUtils/index.html"
+  }
+];
